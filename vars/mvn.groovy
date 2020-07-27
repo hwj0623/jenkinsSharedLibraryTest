88628516c20ec5099f2de931545acd06d8cde938
kimmyeonghwan <kmh@nhn.com>
2020-07-27 17:21:02 +0900

    mvn.groovy 수정

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,18 +1,11 @@
 //vars/mavne_build.groovy
 
+properties([parameters([string(defaultValue: 'test', description: 'test or verify', name: 'testType', trim: false)])])
+
 def call(){
 
     node{
-
         checkout scm
-
-        def testType
-
-        stage("input"){
-            def userInput = input message: 'Choose test type', parameters: [string(defaultValue: 'test', description: 'test or verify', name: 'testType', trim: false)], submitterParameter: 'store'
-          testType = userInput.testType
-        }
-
         stage('build'){
             withEnv(["PATH+MAVEN=${tool 'mvn-3.6.2'}/bin", 
             "JAVA_HOME=${tool  'openjdk8'}", 
@@ -26,7 +19,6 @@ def call(){
             junit 'target/surefire-reports/*.xml'
             jacoco execPattern: 'target/**.exec'
             addShortText background: '', borderColor: '', color: '', link: '', text: testType
-
         }
     }
 
